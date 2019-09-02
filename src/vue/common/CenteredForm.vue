4aeec5eb73f2b5c41bef41d2241842735db96376
Artem Ershov <artemershov1@gmail.com>
2019-09-02 16:47:39 +0300

    CenteredForm wrapper component

---
+++ src/vue/common/CenteredForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="centered-form">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'centered-form',
+}
+</script>
+
+<style lang="sass">
+.centered-form {
+  text-align: center;
+}
+
+.centered-form input,
+.centered-form textarea {
+  text-align: center;
+}
+
+.centered-form input ~ span,
+.centered-form textarea ~ span {
+  width: 100%;
+}
+
+.centered-form button {
+  margin: auto;
+}
+</style>
